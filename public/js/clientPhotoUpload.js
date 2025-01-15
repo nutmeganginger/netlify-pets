502b244998e3d3b81cfc739ccd72b4e069adeef1
Megan <megan.mcclain@opscompass.com>
2025-01-15 15:04:58 -0800

    fixing this?

---
+++ public/js/clientPhotoUpload.js
@@ -0,0 +1,36 @@
+let serverSignature
+let serverTimestamp
+
+
+async function getSignature() {
+  const signaturePromise = await fetch("/.netlify/functions/getSignature")
+  const theResponse = await signaturePromise.json()
+  console.log(theResponse)
+  serverSignature = theResponse.signature
+  serverTimestamp = theResponse.timestamp
+}
+
+getSignature()
+
+document.querySelector("#file-field").addEventListener("change", async function () {
+
+  const data = new FormData()
+  data.append("file", document.querySelector("file-field").files[0])
+  data.append("api_key", "568646442124532")
+  data.append("signature", serverSignature)
+  data.append("timestamp", serverTimestamp)
+
+  // send to cloudinary
+
+  const cloudinaryResponse = await axios.post("https://api.cloudinary.com/v1_1/dkdbg6fvl/auto/upload", data, {
+    headers: { "Content-Type": "multipart/form-data" },
+    onUploadProgress: function (e) {
+      console.log(e.loaded / e.total)
+    }
+  })
+
+  console.log(cloudinaryResponse.data)
+  document.querySelector("#photo-preview").innerHTML = `<img src="https://res.cloudinary.com/dkdbg6fvl/image/upload/w_190,h_190,c_fill/${cloudinaryResponse.data.public_id}.jpg" />`
+
+
+})
